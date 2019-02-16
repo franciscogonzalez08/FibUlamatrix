20864cf1aa0a970c22d5e85f20c7dc235ddb461c
drophy <44790265+drophy@users.noreply.github.com>
2019-02-16 13:42:47 -0600

    Add comments

--- src/tarea3/FibUlamatrix.java
+++ src/tarea3/FibUlamatrix.java
@@ -3,23 +3,28 @@ package tarea3;
 import javax.swing.JOptionPane;
 
 public class FibUlamatrix {
-	private static String strulam = "";
+	private static String strulam = ""; // temporarily stores Ulam series
 	private static int fibulamatrix[][];
+	
 	public static void main(String[] args) {
-		// Hello World question mark
+		// Read user input
 		int n = Integer.parseInt(JOptionPane.showInputDialog("Dame n:"));
+		// Figure out Ulam series' members and length
 		ulam(n);
 		String arrUlam[] = strulam.split(" ");
 		int rows = arrUlam.length;
 		fibulamatrix = new int[rows][];
+		
+		// We'll reserve space for the pseudo-fibonaccis and store them
 		int numUlam;
 		for(int i = 0; i <rows; i++) {
 			numUlam = Integer.parseInt(arrUlam[i]);
-			if(numUlam > 20)
-				numUlam = 20;
+			if(numUlam > 20) numUlam = 20; // we're asked not to calculate over 20 tho so yeah
 			fibulamatrix[i] = new int[numUlam];
-			fibonacci(numUlam, i);
+			fibonacci(numUlam, i); // this calculates the fibos and stores them in the fibulamatrix
 		}
+		
+		// We'll store our desired output in a string because it's pedagogic or something
 		int i, j;
 		String strFibulamatrix = "Fibulamatrix con N = " + n + ":\n";
 		for(i = 0; i < rows; i++) {
@@ -31,16 +36,24 @@ public class FibUlamatrix {
 			strFibulamatrix += numUlam + "] ";
 			if(numUlam > 20)
 				numUlam = 20;
+			
 			for(j = 0; j < numUlam - 1; j++) {
 				strFibulamatrix += fibulamatrix[i][j] + " ";
 			}
 			strFibulamatrix += fibulamatrix[i][j];
+			
+			// We'll see if we truncated the fibos because we had more than 20
 			if(numUlam != Integer.parseInt(arrUlam[i]))
 				strFibulamatrix += " ...";
+			
 			strFibulamatrix += "\n";
 		}
+		
+		// And we can finally print this to the console #yay
 		System.out.print(strFibulamatrix);
 	}
+	
+	// Generates Ulam series and stores them in strulam
 	public static void ulam(int n) {
 		strulam += n;
 		while(n != 1) {
@@ -51,22 +64,22 @@ public class FibUlamatrix {
 			strulam = strulam + " " + n;
 			} 
 	}
+	
+	// Generates ulam pseudo-fibos; having row and row+1 as our first 2 fibos
 	public static void fibonacci(int ulam, int row) {
 		if(ulam == 1)
 			fibulamatrix[row][0] = row;
 		else {
 			int fib1 = row;
 			int fib2 = row + 1;
-			//System.out.printf("%d ", fib1);
 			fibulamatrix[row][0] = fib1; 
 			ulam--;
-			//System.out.printf("%d ", fib2);
 			fibulamatrix[row][1] = fib2;
 			ulam--;
+			
 			int temp;
 			for(int i = 0; i < ulam; i++) {
 				temp = fib1 + fib2;
-				//System.out.printf("%d ", temp);
 				fibulamatrix[row][i + 2] = temp;
 				fib1 = fib2;
 				fib2 = temp;
